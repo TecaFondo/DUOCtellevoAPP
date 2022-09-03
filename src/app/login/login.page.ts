1adcd65cba76e3bee83ad6dfc581d1a063b30376
TecaFondo <carlos.kohler.r@mail.pucv.cl>
2022-09-03 17:01:54 -0400

    Se agrega un validador de existencia de datos en campos de inicio de sesión

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Platform } from '@ionic/angular';
+import { Validators, FormControl, FormBuilder,FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  ios: boolean; //esto es para detectar cuando un dispositivo es iOS
+  android: boolean; //same with android
 
-  constructor() { }
-
-  ngOnInit() {
+  usuario={
+    nombre:'',
+    password:''
   }
-
+  constructor(public platform: Platform) { //valida tipo dispositivo (android o ios)
+    this.ios = platform.is('ios');
+    this.android = platform.is('android');
 }
+
+onSubmitTemplate(){ //Muestra datos de usuario (si son correctos <cumplen validacion>)
+  console.log('Form Submited');
+  console.log(this.usuario);
+}
+}
+
