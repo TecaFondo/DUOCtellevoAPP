b5e4a65110ef8c27a94d3c8f57e56e05ab832af5
TecaFondo <carlos.kohler.r@mail.pucv.cl>
2022-09-03 19:16:34 -0400

    Linkea pag, pero no manda datos necesarios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,9 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  usua: any;
+constructor(private route: ActivatedRoute) {}
+
+
 
 }
