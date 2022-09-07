95dcf1b2ccde14720c40224d87a45395f50ff7e4
tecafondo <carlos.kohler.r@mail.pucv.cl>
2022-09-07 15:10:28 -0400

    Se realizan ultimas modificaciones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { createAnimation } from "@ionic/core";
+import { ElementRef } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,10 +11,31 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild("Logo") Logo:ElementRef;
+
+
 
   usuario: any;
+  
 constructor(private route: ActivatedRoute) {
   this.usuario = this.route.snapshot.paramMap.get('nombre')
+  
+}
+
+ngAfterViewInit(){
+  
+  const animation = createAnimation()
+  .addElement(this.Logo.nativeElement)
+  .easing("ease-in-out")
+  .duration(1000)
+  .direction("alternate")
+  .iterations(Infinity)
+  .keyframes([
+    { offset: 0, transform: "scale(1)", opacity: "1" },
+    { offset: 1, transform: "scale(1.5)", opacity: "0.5" }
+  ]);
+
+animation.play();
 }
 
 
